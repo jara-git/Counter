9af483e56e5e6181a3d8050e345768a9edc279cb
solar <jarabelmonte@gmail.com>
2020-04-02 19:27:35 +0200

    corrección vista números en counter

--- main.js
+++ main.js
@@ -1,77 +1,77 @@
 // ACTIONS
 const addAction = {
-    type: 'ADD',
+   type: "ADD"
  };
-
- const addAction = {
-    type: 'ADDTEN',
+ 
+ const addTenAction = {
+   type: "ADDTEN"
  };
  
  const removeAction = {
-    type: 'REMOVE',
+   type: "REMOVE"
  };
-
- const removeActionTen = {
-    type: 'REMOVETEN',
+ 
+ const removeTenAction = {
+   type: "REMOVETEN"
  };
-
- const reset = {
-    type: 'RESET',
+ 
+ const resetAction = {
+   type: "RESET"
  };
  
  // REDUCER
  const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-        case 'ADD':
-            return state + 1;
-        case 'ADDTEN':
-            return state + 10;
-        case 'REMOVE':
-            return state - 1;
-        case 'REMOVETEN':
-            return state - 10;
-        case 'RESET':
-            return state =0;
-        default:
-            return state;
-    }
- }
+   switch (action.type) {
+     case "ADD":
+       return state + 1;
+     case "REMOVE":
+       return state - 1;
+     case "ADDTEN":
+       return state + 10;
+     case "REMOVETEN":
+       return state - 10;
+     case "RESET":
+       return (state = 0);
+     default:
+       return state;
+   }
+ };
  
  // STORE
  const { createStore } = Redux;
  const store = createStore(counterReducer);
  
  // MAIN
- const renderStore = document.getElementById('render-store');
+ const renderStore = document.getElementById("render-store");
  const render = () => {
-    renderStore.innerHTML = store.getState();
- }
+   renderStore.innerHTML = store.getState();
+ };
  
  store.subscribe(render);
  render();
  
- const add = document.getElementById('add');
- add.addEventListener('click', () => {
-    store.dispatch(addAction)
- });
-
- const add = document.getElementById('addTen');
- add.addEventListener('click', () => {
-    store.dispatch(addActionTen)
+ const add = document.getElementById("add");
+ add.addEventListener("click", () => {
+   store.dispatch(addAction);
  });
  
- const remove = document.getElementById('remove');
- remove.addEventListener('click', () => {
-    store.dispatch(removeAction)
+ const addTen = document.getElementById("addTen");
+ addTen.addEventListener("click", () => {
+   store.dispatch(addTenAction);
  });
-
- const remove = document.getElementById('removeTen');
- remove.addEventListener('click', () => {
-    store.dispatch(removeActionTen)
+ 
+ const remove = document.getElementById("remove");
+ remove.addEventListener("click", () => {
+   store.dispatch(removeAction);
  });
-
- const remove = document.getElementById('reset');
- remove.addEventListener('click', () => {
-    store.dispatch(removeActionReset)
+ 
+ const removeTen = document.getElementById("removeTen");
+ removeTen.addEventListener("click", () => {
+   store.dispatch(removeTenAction);
+ });
+ 
+ const reset = document.getElementById("reset");
+ reset.addEventListener("click", () => {
+   store.dispatch(resetAction);
  });
  
